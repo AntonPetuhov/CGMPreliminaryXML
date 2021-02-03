21eb9ef75b6341192b2139889b24666e200ccba4
AntonPetuhov <petukhov.petukhov@gmail.com>
2021-02-03 17:31:45 +0300

    create service

---
+++ CGM Preliminary XML/Service1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGM_Preliminary_XML
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        public static void WriteToLogFile(string message)
+        {
+
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            WriteToLogFile("Service is running");
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
